db6f12ce58460248f833ac316f2e4dbf0357729e
Anthony Williams <hi@antw.io>
2014-07-18 14:48:46 +0100

    Create merit-convergence
    Models price convergence. See quintel/merit#104.

---
+++ Gemfile
@@ -0,0 +1,8 @@
+# A sample Gemfile
+source 'https://rubygems.org'
+
+gem 'quintel_merit', github: 'quintel/merit', branch: 'convergence'
+
+group :development, :test do
+  gem 'rspec'
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,19 @@
+require 'rspec'
+require_relative '../lib/merit/convergence'
+
+Dir['./spec/support/**/*.rb'].map do |file|
+  require file unless file.end_with?('coverage.rb')
+end
+
+RSpec.configure do |config|
+  # Use only the new "expect" syntax.
+  config.expect_with(:rspec) { |c| c.syntax = :expect }
+
+  # Tries to find examples / groups with the focus tag, and runs them. If no
+  # examples are focues, run everything. Prevents the need to specify
+  # `--tag focus` when you only want to run certain examples.
+  config.filter_run(focus: true)
+  config.run_all_when_everything_filtered = true
+
+  config.expose_dsl_globally = true
+end
